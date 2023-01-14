5aafc8cf83e9d20bd725054f417c08b8941aae4f
Bora Soylu <mail@borasoylu.com>
2023-01-14 13:55:47 +0300

    navbar active tab done

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,22 @@
 import Link from 'next/link';
 import { useState } from 'react';
+import { useRouter } from 'next/router';
+import styles from '../styles/Header.module.css';
+
 export const Header = () => {
+  // Couldn't do this with @apply
+  const activeTab =
+    'text-gray-800 dark:text-white  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium';
+  const inactiveTab =
+    'text-gray-300  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium';
+
+  const router = useRouter();
   const [showMenu, setShowMenu] = useState(false);
   const handleBurgerBtn = () => {};
   return (
     <div>
       <nav className="bg-white dark:bg-gray-800  shadow py-4 ">
-        <div className="px-8 mx-auto max-w-7xl">
+        <div className=" px-8 mx-auto max-w-7xl">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
               <div className="test flex items-center justify-between gap-3">
@@ -35,53 +45,53 @@ export const Header = () => {
               </div>
               <div className="hidden md:block">
                 <div className="flex items-baseline ml-10 space-x-4">
-                  <Link
-                    className="text-gray-300  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium"
-                    href="/#"
-                  >
+                  <Link className={router.pathname == '/' ? activeTab : inactiveTab} href="/">
                     Home
                   </Link>
                   <Link
-                    className="text-gray-800 dark:text-white  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium"
-                    href="/#"
+                    className={router.pathname == '/about' ? activeTab : inactiveTab}
+                    href="/about"
                   >
                     About
                   </Link>
-                  <Link
-                    className="text-gray-300  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium"
-                    href="/#"
-                  >
+                  <Link className={router.pathname == '/faq' ? activeTab : inactiveTab} href="/faq">
                     FAQ
                   </Link>
                   <Link
-                    className="text-gray-300  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium"
-                    href="/#"
+                    className={router.pathname == '/contact' ? activeTab : inactiveTab}
+                    href="/contact"
                   >
-                    Contact
+                    Contact Me
                   </Link>
                 </div>
               </div>
             </div>
-            <div className="hidden md:block">
-              <div className="flex -mr-2 md:block">
-                <form className="flex flex-col justify-center w-3/4 max-w-sm space-y-3 md:flex-row md:w-full md:space-x-3 md:space-y-0">
-                  <div className=" relative ">
-                    <input
-                      type="text"
-                      id='"form-subscribe-Search'
-                      className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                      placeholder="components"
-                    />
-                  </div>
-                  <button
-                    className="flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200"
-                    type="submit"
-                  >
-                    Search
-                  </button>
-                </form>
+            <div className="flex gap-7">
+              <div className="flex justify-end gap-2 items-center">
+                <div>lang</div>
+                <div>dark</div>
               </div>
-              <div className="flex items-center ml-4 md:ml-6"></div>
+              <div className="hidden md:block">
+                <div className="flex -mr-2 md:block">
+                  <form className="flex flex-col justify-center w-3/4 max-w-sm space-y-3 md:flex-row md:w-full md:space-x-3 md:space-y-0">
+                    <div className=" relative ">
+                      <input
+                        type="text"
+                        id='"form-subscribe-Search'
+                        className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                        placeholder="components"
+                      />
+                    </div>
+                    <button
+                      className="flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200"
+                      type="submit"
+                    >
+                      Search
+                    </button>
+                  </form>
+                </div>
+                <div className="flex items-center ml-4 md:ml-6"></div>
+              </div>
             </div>
           </div>
         </div>
