d8e4bd2931bb45dbf4badfbb48a8d84e208db345
Bora Soylu <mail@borasoylu.com>
2023-01-14 12:31:00 +0300

    TODO header Add logo Code currently selected page

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
+import { useState } from 'react';
 export const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
   const handleBurgerBtn = () => {};
   return (
     <div>
@@ -7,24 +9,30 @@ export const Header = () => {
         <div className="px-8 mx-auto max-w-7xl">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
-              <div className="flex -mr-2 md:hidden">
-                <button className="text-gray-800 dark:text-white hover:text-gray-300 inline-flex items-center justify-center p-2 rounded-md focus:outline-none">
-                  <svg
-                    width="20"
-                    height="20"
-                    fill="currentColor"
-                    className="w-8 h-8"
-                    viewBox="0 0 1792 1792"
-                    xmlns="http://www.w3.org/2000/svg"
+              <div className="test flex items-center justify-between gap-3">
+                <div className="flex -mr-2 md:hidden">
+                  <button
+                    onClick={() => {
+                      setShowMenu(!showMenu);
+                    }}
+                    className="text-gray-800 dark:text-white hover:text-gray-300 inline-flex items-center justify-center p-2 rounded-md focus:outline-none"
                   >
-                    <path d="M1664 1344v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45z"></path>
-                  </svg>
-                  <div>test</div>
-                </button>
+                    <svg
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      className="w-8 h-8"
+                      viewBox="0 0 1792 1792"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M1664 1344v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45z"></path>
+                    </svg>
+                  </button>
+                </div>
+                <Link className="flex-shrink-0" href="/">
+                  <p>LOGO</p>
+                </Link>
               </div>
-              <Link className="flex-shrink-0" href="/">
-                <p>LOGO PHONE</p>
-              </Link>
               <div className="hidden md:block">
                 <div className="flex items-baseline ml-10 space-x-4">
                   <Link
@@ -37,13 +45,13 @@ export const Header = () => {
                     className="text-gray-800 dark:text-white  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium"
                     href="/#"
                   >
-                    FAQ
+                    About
                   </Link>
                   <Link
                     className="text-gray-300  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium"
                     href="/#"
                   >
-                    Content
+                    FAQ
                   </Link>
                   <Link
                     className="text-gray-300  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium"
@@ -77,7 +85,7 @@ export const Header = () => {
             </div>
           </div>
         </div>
-        <div className="md:hidden">
+        <div className={`md:hidden ${showMenu ? '' : 'hidden'}`}>
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
             <Link
               className="text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium"
