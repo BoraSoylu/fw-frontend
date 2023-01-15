c568002fdd575de50a66ae55a81e64a5fe45c778
Bora <mail@borasoylu.com>
2023-01-15 17:06:05 +0300

    env file and started wallet url fetching

--- pages/[address].tsx
+++ pages/[address].tsx
@@ -1,10 +1,20 @@
-import { useRouter } from 'next/router'
+import { GetServerSidePropsContext } from 'next';
+import { useRouter } from 'next/router';
 
 const Post = () => {
-  const router = useRouter()
-  const { address } = router.query
+  const router = useRouter();
+  const { address } = router.query;
 
-  return <p>Post: {address}</p>
+  return <p>Post: {address}</p>;
+};
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const res = await fetch(`https://www.farazywallet.com/wallet?address=${context.params.address}`);
+  const data = await res.json();
+
+  return {
+    props: { data }, // will be passed to the page component as props
+  };
 }
 
-export default Post
+export default Post;
