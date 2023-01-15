dab3c1a1dbca2005654aa760ffc0d65c065ba3ce
Bora <mail@borasoylu.com>
2023-01-15 17:37:25 +0300

    started getting all coins fetch

--- pages/[address].tsx
+++ pages/[address].tsx
@@ -1,19 +1,28 @@
 import { GetServerSidePropsContext } from 'next';
 import { useRouter } from 'next/router';
+import useSWR from 'swr';
 
-const Post = () => {
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+const Post = ({ walletData }: any) => {
   const router = useRouter();
-  const { address } = router.query;
+  const { data, error, isLoading } = useSWR('/api/user/123', fetcher);
 
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  const { address } = router.query;
+  console.log(data);
   return <p>Post: {address}</p>;
 };
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const res = await fetch(`https://www.farazywallet.com/wallet?address=${context.params.address}`);
-  const data = await res.json();
+  const address = context.params?.address;
+  const res = await fetch(`https://www.farazywallet.com/api/v1/wallet?address=${address}`);
+  const walletData = await res.json();
 
   return {
-    props: { data }, // will be passed to the page component as props
+    props: { walletData }, // will be passed to the page component as props
   };
 }
 
