541bb5b0bbbb6ade7e8489d020559584150e3148
Bora <mail@borasoylu.com>
2023-01-21 13:51:29 +0300

    Restarted wallet view from

--- components/ViewWalletForm/ViewWalletFrom.tsx
+++ components/ViewWalletForm/ViewWalletFrom.tsx
@@ -1,34 +1,8 @@
 import { useEffect, useState } from 'react';
 import useSWR from 'swr';
 
-export default function ViewWalletFrom({ walletData }: any) {
-  const [currentPrice, setCurrentPrice] = useState(null);
-  const [vs_currencies, setVs_currencies] = useState('usd');
-  useEffect(() => {
-    let SimplePriceString: string = 'https://api.coingecko.com/api/v3/simple/price?ids=';
-    Object.keys(walletData.contents).forEach((coin) => {
-      SimplePriceString += walletData.contents[coin].id + '%2C';
-    });
-    SimplePriceString = `${SimplePriceString.slice(0, -3)}&vs_currencies=${vs_currencies}`;
-    const fetcher = (url: any) => fetch(url).then((res) => res.json());
-    fetch(SimplePriceString)
-      .then((res) => res.json())
-      .then((json) => {
-        setCurrentPrice(json);
-      });
-  }, []);
+type DisplayWalletStanding = {};
 
-  if (currentPrice === null) {
-    return <div>loading</div>;
-  } else {
-    return (
-      <div>
-        <p>Wallet: {JSON.stringify(walletData)}</p>
-        <br />
-        <p>{`Bought at: ${JSON.stringify(currentPrice)}`}</p>
-        <p>{`Currently:${JSON.stringify(currentPrice)}`}</p>
-        <br />
-      </div>
-    );
-  }
+export default function ViewWalletFrom({ walletData }: any) {
+  return <div>ViewWalletForm</div>;
 }
--- pages/[address].tsx
+++ pages/[address].tsx
@@ -6,37 +6,21 @@ import ViewWalletFrom from '../components/ViewWalletForm/ViewWalletFrom';
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-const Post = ({ walletData }: any) => {
+const WalletView = ({ walletData }: any) => {
   const router = useRouter();
+  const { address } = router.query;
   if (walletData instanceof Error) {
     return <div>{walletData.message}</div>;
   } else {
     return (
-      <div>
-        <ViewWalletFrom walletData={walletData} />
-      </div>
+      <>
+        <p>Address is: {address}</p>
+        <ViewWalletFrom />
+      </>
     );
   }
 };
 
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  try {
-    const address = context.params?.address;
-    if (typeof address !== 'string' || !/^[123456789ABCDEFGHJKLMNPRSTUVWXYZ]{10}$/.test(address)) {
-      throw Error('Invalid Address parameter!');
-    }
-    const res = await fetch(`https://www.farazywallet.com/api/v1/wallet?address=${address}`);
-    const walletData = await res.json();
+// export async function getServerSideProps(context: GetServerSidePropsContext) {s}
 
-    return {
-      props: { walletData }, // will be passed to the page component as props
-    };
-  } catch (error) {
-    const walletData = error;
-    return {
-      props: { walletData }, // will be passed to the page component as props
-    };
-  }
-}
-
-export default Post;
+export default WalletView;
