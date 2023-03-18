912ab9c3886b5f3c3345f41f7d89705bf81bd295
Bora Soylu <mail@borasoylu.com>
2023-03-18 20:52:03 +0300

    dark mode added to home page

--- components/CreateWalletForm/CreateWalletForm.tsx
+++ components/CreateWalletForm/CreateWalletForm.tsx
@@ -15,22 +15,24 @@ export default function CreateWalletForm({
 }) {
   if (loading) return <div>loading</div>;
   return (
-    <div className="container">
+    <div className="container dark:bg-gray-800 p-3 rounded-xl">
       <form className="flex flex-col justify-center w-fit space-y-3 md:flex-row md:space-x-3 md:space-y-0">
-        <div className=" relative w-64 ">{<CoinSearchBox allCoins={allCoins} />}</div>
-        <div className=" relative w-64 ">
+        <div className="relative w-64">
+          <CoinSearchBox allCoins={allCoins} />
+        </div>
+        <div className="relative w-64">
           <input
             type="text"
             id="form-create-wallet"
-            className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-green-600 focus:border-transparent"
+            className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white dark:bg-gray-800 text-gray-700 dark:text-white placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-green-600 focus:border-transparent"
             placeholder="Amount"
           />
         </div>
-        <div className=" relative w-64 ">
+        <div className="relative w-64">
           <input
             type="text"
             id="form-create-wallet"
-            className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-green-600 focus:border-transparent"
+            className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white dark:bg-gray-800 text-gray-700 dark:text-white placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-green-600 focus:border-transparent"
             placeholder={`In ${selectedCurrency.toUpperCase()}`}
           />
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,21 @@
 import Head from 'next/head';
 import Link from 'next/link';
+
 export default function Home() {
   return (
-    <div>
+    <div className="dark:bg-gray-900">
       <Head>
         <title>Farazy Wallet</title>
         <meta name="keywords" content="TODO" />
       </Head>
 
-      <div className="relative h-screen overflow-hidden bg-white">
+      <div className="relative h-screen overflow-hidden bg-white dark:bg-gray-900">
         <div className="h-full mx-auto max-w-7xl">
-          <div className="relative z-10 h-full pb-8 bg-white sm:pb-16 md:pb-20 lg:w-full lg:pb-28 xl:pb-32">
+          <div className="relative z-10 h-full pb-8 bg-white sm:pb-16 md:pb-20 lg:w-full lg:pb-28 xl:pb-32 dark:bg-gray-900">
             <main className="h-full px-4 mx-auto mt-10 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
               <div className="flex flex-col items-start justify-between w-full h-full md:flex-row md:justify-start md:h-1/2">
-                <div className="z-20 flex flex-col items-center justify-start w-full h-full text-left md:z-30 md:w-1/2 md:items-start md:justify-center">
-                  <h1 className="text-6xl font-extrabold tracking-tight text-gray-900 titleHome ">
+                <div className="z-20 flex flex-col items-center justify-start w-full h-full text-left text-gray-900 dark:text-gray-200 md:z-30 md:w-1/2 md:items-start md:justify-center">
+                  <h1 className="text-6xl font-extrabold tracking-tight titleHome ">
                     <span className="flex w-full m-auto text-green-600">Farazy Wallet</span>
                     <span className="block font-bold xl:inline">
                       <span className="absolute">Fast and Easy</span>
@@ -22,7 +23,7 @@ export default function Home() {
                       Mock Cryptowallet
                     </span>
                   </h1>
-                  <h2 className="mt-3 text-lg text-gray-500 sm:mt-5 sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                  <h2 className="mt-3 text-lg text-gray-500 dark:text-gray-400 sm:mt-5 sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
                     Create an imaginary wallet without logging in or providing any personal
                     information! Access it anytime anywhere by writing down a simple address like
                     this:{' '}
@@ -34,19 +35,19 @@ export default function Home() {
                   </h2>
                   <div className="w-full mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                     <div className="rounded-md shadow">
-                      <a
+                      <Link
                         className="flex items-center justify-center w-full px-4 px-8 py-2 py-3 text-base font-medium text-white bg-green-600 rounded-md hover:bg-green-700"
                         href="/started"
                       >
                         Create Wallet
-                      </a>
+                      </Link>
                     </div>
                     <div className="mt-3 sm:mt-0 sm:ml-3">
                       <a
                         target="_blank"
                         rel="noreferrer"
                         href="https://github.com/Charlie85270/tail-kit"
-                        className="flex items-center justify-center w-full px-4 px-8 py-2 py-3 text-base font-medium text-gray-800 bg-gray-100 rounded-md hover:bg-gray-200"
+                        className="flex items-center justify-center w-full px-4 px-8 py-2 py-3 text-base font-medium text-gray-800 dark:text-gray-200 bg-gray-100 dark:bg-gray-800 rounded-md hover:bg-gray-200 dark:hover:bg-gray-700"
                       >
                         <span className="mr-2">Learn More</span>
                         <svg
@@ -66,7 +67,7 @@ export default function Home() {
                       </a>
                     </div>
                   </div>
-                  <div className="mt-4">
+                  <div className="mt-4 text-gray-900 dark:text-gray-200">
                     Contact me for more information
                     <span className="underline cursor-pointer ml-2 font-semibold">
                       borasoylu.com
